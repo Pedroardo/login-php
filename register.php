e9fe84c8907a0cc8197c37bf21cc51bd7b099c9e
Pedro <pedroardo10@gmail.com>
2025-12-05 12:50:07 +0700

    validation on register for a better data input

--- register.php
+++ register.php
@@ -6,23 +6,39 @@ $register_message = "";
 
 if(isset($_SESSION["is_login"])){
     header("location: dashboard.php");
+    exit;
 }
 
 if(isset($_POST['register'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $hash_password = hash("sha256", $password);
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
-    try {
-    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hash_password')";
-    IF($db->query($sql)){
-        $register_message = "Registrasi Berhasil, Silakhkan Login";
-    }else {
-        $register_message ="Registrasi Gagal";
+    // Validasi form kosong
+    if(empty($username) || empty($password)){
+        $register_message = "Username dan Password wajib diisi!";
+    } 
+    elseif(strlen($password) < 6){
+        $register_message = "Password minimal 6 karakter!";
+    } else {
+        // Validasi unik username
+        $sql_check = "SELECT * FROM users WHERE username = '$username'";
+        $result = $db->query($sql_check);
+
+        if($result->num_rows > 0){
+            $register_message = "Username sudah terdaftar!";
+        } else {
+            // Lolos semua validasi → simpan ke database
+            $hash_password = hash("sha256", $password);
+            $sql_insert = "INSERT INTO users (username, password) VALUES ('$username', '$hash_password')";
+            
+            if($db->query($sql_insert)){
+                $register_message = "Registrasi Berhasil, Silakan Login.";
+            } else {
+                $register_message = "Registrasi gagal.";
+            }
+        }
     }
-    } catch (mysqli_sql_exception $e) {
-        $register_message = $e->getMessage();
-    }
+
     $db->close();
 }
 
@@ -41,8 +57,8 @@ if(isset($_POST['register'])){
     <?php  include "layout/header.html" ?>
     <h2>Daftar Akun</h2>
     <form action="register.php" method="POST">
-        <input type="text" placeholder="username" name="username">
-        <input type="password" placeholder="password" name="password">
+        <input type="text" placeholder="username" name="username" require>
+        <input type="password" placeholder="password" name="password" require>
         <button type="submit" name="register">Daftar Sekarang</button>
     </form>
     <p><?=$register_message?></p>
