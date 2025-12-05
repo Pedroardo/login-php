28404ccb8ba6aecee081242b1cb1dc4e898be222
Pedro <pedroardo10@gmail.com>
2025-12-05 11:52:23 +0700

    login data match with database

--- login.php
+++ login.php
@@ -1,9 +1,18 @@
 <?php 
-    include("service/db.php");
+include("service/db.php");
 
- if(isset($_POST['login'])){
+if(isset($_POST['login'])){
     $username = $_POST['username'];
-    
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $db->query($sql);
+
+    if($result->num_rows > 0){
+        $data = $result->fetch_assoc();
+    }else {
+        echo "Login Gagal";
+    }
  }
 
 ?>
